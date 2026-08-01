84cb67c6b5bdf2578ceb0e939808374f597b3d56
LetC0de <abhishekjiiofficial86@gmail.com>
2026-08-01 19:08:37 +0530

    implement file upload functionality with temporary PDF file creation

--- src/upload/controller.py
+++ src/upload/controller.py
@@ -1,6 +1,26 @@
 from fastapi import UploadFile
+import tempfile
+
 
 async def upload_doc(file: UploadFile):
-    return {  "file name": file.filename,
-                    "content type": file.content_type,
-                    "message": "File uploaded successfully"}
+
+    # Create a temporary PDF file
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".pdf"
+    ) as temp_file:
+
+        # Read uploaded PDF
+        contents = await file.read()
+
+        # Write into temporary file
+        temp_file.write(contents)
+
+        # Get temporary file path
+        temp_file_path = temp_file.name
+
+    return {
+        "message": "Temporary file created successfully",
+        "original_filename": file.filename,
+        "temp_file_path": temp_file_path
+    }
