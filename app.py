27332eb0a1e9b0cd15e867dab82d6e529851a950
LetC0de <abhishekjiiofficial86@gmail.com>
2026-08-01 20:03:10 +0530

    Implement lifespan context manager to create collection on app startup

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from src.qdrant.collection import create_collection
 from src.upload.router import upload_router
 
-app = FastAPI(title = "Enterprise Knowledge Asistant")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_collection()
+    yield
+
+
+app = FastAPI(title="Enterprise Knowledge Asistant", lifespan=lifespan)
 
 
 app.include_router(upload_router)
